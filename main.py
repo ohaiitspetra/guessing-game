1efc015cf65c893a0fc188a400482132dc86abfd
ohaiitspetra <43091834+ohaiitspetra@users.noreply.github.com>
2018-11-15 20:58:21 -0500

    Completed Game

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import random
 
 def mini_game(a,b):
-health_points=3
-guess= input("Guess the number bimbo:")
-secret_number= random.randint(a,b)
+  health_points=3
+  guess= input("Guess the number bimbo:")
+  secret_number= random.randint(a,b)
+if guess>!=secret_number:
+  return ("Woah buddy, too high.")
+elif guess<!=secret_number:
+  ("Well someone's down in the dumps, that's a pretty low number.")
 if guess==secret_number:
   print("Yay you guessed it!")
   print("Level 2!")
@@ -21,9 +25,17 @@ elif health_points>0:
     print("Who are you again?")
     secret_3= (a,b+5)
     guess_3= ("Round 3!")
+  if guess_2>!=secret_2:
+  return ("Woah buddy, too high.")
+elif guess_2<!=secret_2:
+  print ("Someone's down in the dumpster, too low.")
   
     if guess==secret_3:
       print("YOU WON!!! HAVE A COOKIE")
 else: health_points == 0:
   print("Game over")
+  if guess_3>!=secret_3:
+  return ("Woah buddy, too high.")
+elif guess_3<!=secret_3:
+  print ("Ya missed, too low. Cheer up buddy.")
 
